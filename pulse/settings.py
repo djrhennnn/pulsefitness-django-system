1b8c144b21770a68e58d202925fdf543a742a911
RHEN LEWIS N. DE JESUS <rhenlewis.dejesus@gsfe.tupcavite.edu.ph>
2026-03-30 10:39:36 +0800

    Update settings.py

--- pulse/settings.py
+++ pulse/settings.py
@@ -94,7 +94,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+# MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
