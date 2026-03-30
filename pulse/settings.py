26715f13c46d5cfeef1d500e03e08be605f132c2
RHEN LEWIS N. DE JESUS <rhenlewis.dejesus@gsfe.tupcavite.edu.ph>
2026-03-30 11:10:23 +0800

    Update settings.py

--- pulse/settings.py
+++ pulse/settings.py
@@ -33,14 +33,8 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
 }
 
-STORAGES = {
-    "default": {
-        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
-    },
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
