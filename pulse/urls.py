18629128a6dd66a44b78efe13d127fefb2f8de7e
RHEN LEWIS N. DE JESUS <rhenlewis.dejesus@gsfe.tupcavite.edu.ph>
2026-03-30 10:51:19 +0800

    Update urls.py

--- pulse/urls.py
+++ pulse/urls.py
@@ -4,6 +4,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('django-admin/', admin.site.urls),   # Django built-in admin kept at /django-admin/
-    path('', include('fitness.urls')),         # All fitness app URLs at root
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('django-admin/', admin.site.urls),
+    path('', include('fitness.urls')),
+]
